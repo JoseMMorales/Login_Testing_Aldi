517956703f1c5f625e3b9438d9ebf2cb65a3cc4e
Jose <josemulamorales33@hotmail.com>
2020-03-01 17:11:36 +0000

    Update Scripts Aldi Login functionality

--- src/Login_Testing/TC4_InvUser_ValPass.java
+++ src/Login_Testing/TC4_InvUser_ValPass.java
@@ -9,43 +9,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TC4_InvUser_ValPass {
 
-	public static void main(String[] args) {
-		// Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "/......./chromedriver");
-		
-		//Launch Chrome and maximise
-		WebDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		
-		driver.get("https://www.aldi.co.uk/");
-		
-		//Wait until homepage is charged and click log in
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		 
-		WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
-		
-		login.click();
-		
-		//Wait until log in page is charged, enter username and password
-		WebDriverWait wait1 = new WebDriverWait(driver, 10);
-		
-		WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
-		
-		username.sendKeys("xxxxxxxxxxx@gmail.com");
-		
-		WebElement password = driver.findElement(By.id("j_password"));
-		
-		password.sendKeys("Testing1989@");
-		
-		//Click Sign button
-		WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
-		
-		sign.click();
-		
-		//Close Browser
-		driver.quit();			
-
-	}
-
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/......./chromedriver");
+    //Launch Chrome and maximise
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.aldi.co.uk/");
+    
+    //Wait until homepage is charged and click log in
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
+    login.click();
+    
+    //Wait until log in page is charged, enter username and password
+    WebDriverWait wait1 = new WebDriverWait(driver, 10);
+    WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
+    username.sendKeys("xxxxxxxxxxx@gmail.com");
+    
+    WebElement password = driver.findElement(By.id("j_password"));
+    password.sendKeys("Testing1989@");
+    
+    //Click Sign button
+    WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
+    sign.click();
+    
+    //Close Browser
+    driver.quit();			
+  }
 }
--- src/Login_Testing/TC9_BlankUser_BlankPass.java
+++ src/Login_Testing/TC9_BlankUser_BlankPass.java
@@ -9,42 +9,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TC9_BlankUser_BlankPass {
 
-	public static void main(String[] args) {
-		// Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
-		
-		//Launch Chrome and maximise
-		WebDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		
-		driver.get("https://www.aldi.co.uk/");
-		
-		//Wait until homepage is charged and click log in
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		 
-		WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
-		
-		login.click();
-		
-		//Wait until log in page is charged, enter username
-		WebDriverWait wait1 = new WebDriverWait(driver, 10);
-				
-		WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
-				
-		username.sendKeys("       ");
-		
-        WebElement password = driver.findElement(By.id("j_password"));
-		
-		password.sendKeys("     ");
-		
-		//Click Sign button
-		WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
-				
-		sign.click();
-				
-		//Close Chrome
-		driver.close();
-	}
-
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
+    //Launch Chrome and maximise
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.aldi.co.uk/");
+    
+    //Wait until homepage is charged and click log in
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
+    login.click();
+    
+    //Wait until log in page is charged, enter username
+    WebDriverWait wait1 = new WebDriverWait(driver, 10);
+    WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
+    username.sendKeys("       ");
+    
+    WebElement password = driver.findElement(By.id("j_password"));
+    password.sendKeys("     ");
+    
+    //Click Sign button
+    WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
+    sign.click();
+        
+    //Close Chrome
+    driver.close();
+  }
 }
