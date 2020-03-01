e2876350bad7a46cf2dfbf2f458f367c42e36ef1
Jose <josemulamorales33@hotmail.com>
2020-03-01 17:04:11 +0000

    Create Scripts Aldi Login functionality

--- src/Login_Testing/TC3_ValUser_BlankPass.java
+++ src/Login_Testing/TC3_ValUser_BlankPass.java
@@ -9,39 +9,28 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TC3_ValUser_BlankPass {
 
-	public static void main(String[] args) {
-		// Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
-		
-		//Launch Chrome and maximise
-		WebDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		
-		driver.get("https://www.aldi.co.uk/");
-		
-		//Wait until homepage is charged and click log in
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		 
-		WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
-		
-		login.click();
-		
-		//Wait until log in page is charged, enter username
-		WebDriverWait wait1 = new WebDriverWait(driver, 10);
-		
-		WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
-		
-		username.sendKeys("Demo.testing1919190@gmail.com");
-		
-		//Click Sign button
-		WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
-		
-		sign.click();
-				
-		//Close Browser
-		driver.close();			
-
-	}
-
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
+    //Launch Chrome and maximise
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.aldi.co.uk/");
+    
+    //Wait until homepage is charged and click log in
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
+    login.click();
+    
+    //Wait until log in page is charged, enter username
+    WebDriverWait wait1 = new WebDriverWait(driver, 10);
+    WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
+    username.sendKeys("Demo.testing1919190@gmail.com");
+    
+    //Click Sign button
+    WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
+    sign.click();
+        
+    //Close Browser
+    driver.close();			
+  }
 }
