17d412d2afb3e29270217d8cfd71a1d491b532ce
Jose <josemulamorales33@hotmail.com>
2020-03-01 16:57:20 +0000

    Create Scripts Aldi Login functionality

--- src/Login_Testing/TC1_ValUser_ValPass_LogOut.java
+++ src/Login_Testing/TC1_ValUser_ValPass_LogOut.java
@@ -9,48 +9,35 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TC1_ValUser_ValPass_LogOut {
 
-	public static void main(String[] args) {
-		// Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
-		
-		//Launch Chrome and maximise
-		WebDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		
-		driver.get("https://www.aldi.co.uk/");
-		
-		//Wait until homepage is charged and click log in
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		 
-		WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
-		
-		login.click();
-		
-		//Wait until log in page is charged, enter username and password
-		WebDriverWait wait1 = new WebDriverWait(driver, 10);
-		
-		WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
-		
-		username.sendKeys("Demo.testing1919190@gmail.com");
-		
-		WebElement password = driver.findElement(By.id("j_password"));
-		
-		password.sendKeys("Testing1989@");
-		
-		//Click Sign button
-		WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
-		
-		sign.click();
-		
-		//Click Log Out button
-		WebElement logout = driver.findElement(By.linkText("Log Out"));
-		
-		logout.click();
-		
-		//Close Browser
-		driver.close();			
-
-	}
-
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
+    //Launch Chrome and maximise
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.aldi.co.uk/");
+    
+    //Wait until homepage is charged and click log in
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log in / Register")));
+    login.click();
+    
+    //Wait until log in page is charged, enter username and password
+    WebDriverWait wait1 = new WebDriverWait(driver, 10);
+    WebElement username = wait1.until(ExpectedConditions.elementToBeClickable(By.id("j_username")));
+    username.sendKeys("Demo.testing1919190@gmail.com");
+    
+    WebElement password = driver.findElement(By.id("j_password"));
+    password.sendKeys("Testing1989@");
+    
+    //Click Sign button
+    WebElement sign = driver.findElement(By.xpath("//*[@id=\"returningCustomerForm\"]/div[4]/button"));
+    sign.click();
+    
+    //Click Log Out button
+    WebElement logout = driver.findElement(By.linkText("Log Out"));
+    logout.click();
+    
+    //Close Browser
+    driver.close();			
+  }
 }
